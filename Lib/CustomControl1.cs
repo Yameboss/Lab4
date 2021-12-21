662bc2e5abe9302f498ae9bb571102b68f789979
Yameboss <95024953+Yameboss@users.noreply.github.com>
2021-12-21 23:18:56 +0300

    Add files via upload

---
+++ Lib/CustomControl1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lib
+{
+
+    public class CustomControl1 : Control
+    {
+        public static void puz(int[] num, string[] mass)
+        {
+            
+            for (int i = 0; i < num.Length; i++)
+            {
+                num[i] = Convert.ToInt32(mass[i]);
+            }
+            int temp;
+            for (int i = 0; i < num.Length - 1; i++)
+            {
+                for (int j = i + 1; j < num.Length; j++)
+                {
+                    if (num[i] > num[j])
+                    {
+                        temp = num[i];
+                        num[i] = num[j];
+                        num[j] = temp;
+                    }
+                }
+            }
+            
+        }
+        static CustomControl1()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl1), new FrameworkPropertyMetadata(typeof(CustomControl1)));
+        }
+    }
+}
